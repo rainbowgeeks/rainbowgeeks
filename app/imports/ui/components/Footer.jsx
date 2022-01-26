d505c8f5e09f4350f89800036154268a78b0ac87
Jonathan-Ma <jma66@hawaii.edu>
2022-01-25 16:46:57 -1000

    footer with picture

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => {
   const divStyle = { paddingTop: '15px' };
   return (
     <footer>
+      <div class={'footer'}>
+        <div id={'footerPic'}>
+          <h1>Ready to Get Started?</h1>
+          <Button>Get Started</Button>
+        </div>
+      </div>
+
       <div style={divStyle} className="ui center aligned container">
-        <hr />
-        Department of Information and Computer Sciences <br />
-        University of Hawaii<br />
-        Honolulu, HI 96822 <br />
-        <a href="http://ics-software-engineering.github.io/matrp">Template Home Page</a>
+        <p>
+        ©2021-2022 VolunteerAlly. All rights reserved.
+          <a href="https://volunteerally.org/privacy-policy"> Privacy Policy.</a>
+          <a href="https://volunteerally.org/privacy-policy"> Terms & Conditions</a>
+        </p>
       </div>
     </footer>
   );
