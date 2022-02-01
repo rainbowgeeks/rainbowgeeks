f6ebae9352d722c5e6b82b0268859f8e66889f7c
Jonathan-Ma <jma66@hawaii.edu>
2022-01-31 20:25:10 -1000

    footer text position fix

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -6,16 +6,21 @@ const Footer = () => {
   const divStyle = { paddingTop: '15px' };
   return (
     <footer>
-      <div class={'footer'}>
+      <div className={'footer'}>
         <div id={'footerPic'}>
-          <h1>Ready to Get Started?</h1>
-          <Button>Get Started</Button>
+          <div className={'ui equal width grid'}>
+            <div className="column"/>
+            <div className="column">
+              <h1 id={'footerTxt'}>Ready to Get Started?</h1>
+              <Button>Get Started</Button>
+            </div>
+            <div className="column"/>
+          </div>
         </div>
       </div>
-
-      <div style={divStyle} className="ui center aligned container">
+      <div style={divStyle} className="ui equal width grid">
         <p>
-        ©2021-2022 VolunteerAlly. All rights reserved.
+            ©2021-2022 VolunteerAlly. All rights reserved.
           <a href="https://volunteerally.org/privacy-policy"> Privacy Policy.</a>
           <a href="https://volunteerally.org/privacy-policy"> Terms & Conditions</a>
         </p>
