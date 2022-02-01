0a4a78d85b41c1b6f725e6596455a066329e33c8
Isaiah Eusebio <icce@hawaii.edu>
2022-01-31 16:16:23 -1000

    Update AdminProfile.jsx

--- app/imports/ui/pages/AdminProfile.jsx
+++ app/imports/ui/pages/AdminProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Loader, Image, Label, Grid, Segment, Table, Icon } from 'semantic-ui-react';
+import { Loader, Image, Label, Grid, Segment, Table, Icon, Divider } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Stuffs } from '../../api/stuff/StuffCollection';
@@ -7,55 +7,56 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const AdminProfile = ({ stuffs, ready }) => ((ready) ? (
-  <Container id={PAGE_IDS.ADMIN_PROFILE}>
-    <Grid columns={3} celled>
-      <Grid.Column textAlign='center' verticalAlign='top'>
-        <Image src='/images/meteor-logo.png' size='medium' circular/>
-      </Grid.Column>
-      <Grid.Column>
-        <Table celled striped>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell colSpan='2'>Account</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
+  <Grid id={PAGE_IDS.ADMIN_PROFILE} columns={2} celled container>
+    <Grid.Column textAlign='center' width={5}>
+      <Image src='/images/meteor-logo.png' size='medium' circular centered/>
+      <Divider hidden/>
+      <Label size='big' circular>Administrator</Label>
+    </Grid.Column>
+    <Grid.Column width={8}>
+      <Table celled striped>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell colSpan='2'>Account</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
 
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>Name</Table.Cell>
-              <Table.Cell>John Doe</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Email</Table.Cell>
-              <Table.Cell>johndoe@foo.com</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>Address</Table.Cell>
-              <Table.Cell>1234 Dole Street</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>
-                <Icon name='github' /> GitHub
-              </Table.Cell>
-              <Table.Cell>Initial commit</Table.Cell>
-            </Table.Row>
-            <Table.Row>
-              <Table.Cell>
-                <Icon name='twitter' /> Twitter
-              </Table.Cell>
-              <Table.Cell>Initial commit</Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
-      </Grid.Column>
-      <Grid.Column>
-        <Segment>Content</Segment>
-      </Grid.Column>
-      <Grid.Column>
-        <Label size='big' circular>Administrator</Label>
-      </Grid.Column>
-    </Grid>
-  </Container>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>Username</Table.Cell>
+            <Table.Cell>johnd</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Name</Table.Cell>
+            <Table.Cell>John Doe</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Email</Table.Cell>
+            <Table.Cell>johndoe@foo.com</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>Address</Table.Cell>
+            <Table.Cell>1234 Dole Street</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>
+              <Icon name='github' /> GitHub
+            </Table.Cell>
+            <Table.Cell>Initial commit</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>
+              <Icon name='twitter' /> Twitter
+            </Table.Cell>
+            <Table.Cell>Initial commit</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </Grid.Column>
+    <Grid.Column>
+      <Segment>Content</Segment>
+    </Grid.Column>
+  </Grid>
 ) : <Loader active>Getting data</Loader>);
 
 // Require an array of Stuff documents in the props.
