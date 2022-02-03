904637f3753642ae401a1461fb9ff04ccd103aa3
John Suelen <jsuelen@hawaii.edu>
2022-02-03 01:53:51 -1000

    landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
-import { PAGE_IDS } from '../utilities/PageIDs';
+import { Image, Container, Header, Item } from 'semantic-ui-react';
 import Footer from '../components/Footer';
 
 /** A simple static component to render some text for the landing page. */
 const Landing = () => (
   <div>
-    <Grid id={PAGE_IDS.LANDING} verticalAlign='middle' textAlign='center' container>
-
-      <Grid.Column width={4}>
-        <Image size='small' circular src="/images/meteor-logo.png" />
-      </Grid.Column>
-
-      <Grid.Column width={8}>
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
-      </Grid.Column>
-    </Grid>
+    <div className="grad-background">
+      <Container>
+        <Image src='https://volunteerally.org/wp-content/uploads/elementor/thumbs/VA-logo-v5-web-white@2x-pbjpxctj7h8ni6m613deejchpwnvgwi3t9onxij5ts.png' className="logo-center"/>
+        <Item className="h1">
+          A better way to volunteer.
+          <Item className="h2">
+            We connect passionate volunteers with charitable organizations in order to build community. Let us help you easily find service opportunities for organizations in your area of interest.
+          </Item>
+        </Item>
+      </Container>
+    </div>
     <Footer/>
   </div>
-
 );
 
 export default Landing;
