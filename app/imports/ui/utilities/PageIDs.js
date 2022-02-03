27f13b74e966e0ee22ab372e54fa154edb33a4a1
Isaiah Eusebio <icce@hawaii.edu>
2022-02-02 19:19:00 -1000

    Created OrganizationLibrary mockup page

--- app/imports/ui/utilities/PageIDs.js
+++ app/imports/ui/utilities/PageIDs.js
@@ -10,6 +10,7 @@ export const PAGE_IDS = {
   LIST_STUFF_ADMIN: 'list-stuff-admin',
   MANAGE_DATABASE: 'manage-database',
   NOT_FOUND: 'not-found',
+  ORG_LIBRARY_PAGE: 'org-library-page',
   PROFILE_PAGE: 'profile-page',
   EDIT_PROFILE: 'edit-profile',
   SIGN_IN: 'sigin-in',
