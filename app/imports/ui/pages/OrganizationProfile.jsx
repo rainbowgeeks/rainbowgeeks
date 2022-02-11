142c1a5cd8a07f301da5b9dfe2ba046a207104f2
Jonathan-Ma <jma66@hawaii.edu>
2022-02-10 16:34:51 -1000

    logo responsiveness all g

--- app/imports/ui/pages/OrganizationProfile.jsx
+++ app/imports/ui/pages/OrganizationProfile.jsx
@@ -5,10 +5,14 @@ import { PAGE_IDS } from '../utilities/PageIDs';
 const picStyle = { width: '100%', height: '100%' };
 
 const OrganizationProfile = () => (
-  <Container id={PAGE_IDS.PROFILE_PAGE}>
-    <div>
+  <Container id={PAGE_IDS.ORGANIZATION_PROFILE}>
+    <div style={{ position: 'absolute', zIndex: '2', display: 'flex' }} className={'ui container'}>
       <img style={picStyle} src={'https://volunteerally.org/wp-content/uploads/2021/08/va-default-header.png'}/>
     </div>
+    <div>
+      <img style={{ position: 'absolute', zIndex: '3' }} id={'orgLogo'} src={'https://volunteerally.org/wp-content/uploads/2021/08/VA-logo-circle-v5.svg'} width='15%'/>
+    </div>
+    <div style={{ zIndex: '1', position: 'absolute', backgroundColor: '#eee' }} className={'ui container'} id={'greyBack'}/>
   </Container>
 );
 
--- app/imports/ui/utilities/PageIDs.js
+++ app/imports/ui/utilities/PageIDs.js
@@ -16,4 +16,5 @@ export const PAGE_IDS = {
   SIGN_IN: 'sigin-in',
   SIGN_OUT: 'sign-out',
   SIGN_UP: 'sign-up',
+  ORGANIZATION_PROFILE: 'org-profile',
 };
