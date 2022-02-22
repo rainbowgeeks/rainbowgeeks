9c18cb09266f0d02d96ea36719cff19105ce0242
Isaiah Eusebio <icce@hawaii.edu>
2022-02-22 10:12:19 -1000

    Linked AdminProfile to ManageOpportunities

--- app/imports/ui/pages/AdminProfile.jsx
+++ app/imports/ui/pages/AdminProfile.jsx
@@ -84,7 +84,7 @@ const AdminProfile = ({ ready }) => ((ready) ? (
         <Menu.Item header>Site Contents</Menu.Item>
         <Menu.Item as={NavLink} exact to="/manage-user">Manage Users</Menu.Item>
         <Menu.Item as={NavLink} exact to="/manage-org">Manage Organizations</Menu.Item>
-        <Menu.Item>Manage Opportunities</Menu.Item>
+        <Menu.Item as={NavLink} exact to="/manage-opps">Manage Opportunities</Menu.Item>
         <Menu.Item>Site Preferences</Menu.Item>
         <Menu.Item>Analytics</Menu.Item>
       </Menu>
