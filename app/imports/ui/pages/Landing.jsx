ea7dc6e79e85b9a0a43f541c0e167a864f1de269
John Suelen <jsuelen@hawaii.edu>
2022-03-24 00:27:44 -1000

    fix landing
    Made components reactive such that it does not misbehave when zooming in/out

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -19,31 +19,47 @@ const Landing = () => (
         </h1>
       </Container>
     </div>
-    <Grid className="landingBot" columns="2">
-      <h1 className="h1Bot">Dozens of Opportunities for Organizations and Volunteers</h1>
-      <Grid.Column>
-        <Container fluid>
-          <h2 className="h2Bot">
-            Take a look at the various organizations Volunteer Ally works with, each with unique volunteer opportunities offering various dates, times, and locations.
-          </h2>
-          <p className="desc">
-            Volunteer Ally is a non-profit organization match system designed to pair organizations with volunteers. We partner with various organizations in need of passionate volunteers.
-          </p>
-          <p className="desc">
-            Becoming a user is required to ensure committed reliable volunteers for our organizations.
-          </p>
-          <Link to="/signup">
-            <Button className={'ui footer button bot'} size={'massive'}>Join Now!</Button>
-          </Link>
-        </Container>
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://cdn.discordapp.com/attachments/441780757967405067/943854340714676284/unknown.png'></Image>
-      </Grid.Column>
-    </Grid>
+    <Container fluid>
+      <Grid container spacing={2} >
+        <Grid item xs={12}>
+          <h1 className="h1Bot" >Dozens of Opportunities for Organizations and Volunteers</h1>
+        </Grid>
+        <Grid item sm={6}>
+          <Container fluid>
+            <h2 className="h2Bot">
+              <p>Take a look at the various<br/>
+            organizations Volunteer Ally<br/>
+            works with, each with unique<br/>
+            volunteer opportunities offering<br/>
+              various dates, times, and<br/>
+              locations.
+              </p>
+            </h2>
+            <p className="desc">
+            Volunteer Ally is a non-profit organization <br/>
+            match system designed to pair <br/>
+            organizations with volunteers. We partner <br/>
+            with various organizations in need of <br/>
+            passionate volunteers.
+            </p>
+            <p className="desc">
+            Becoming a user is required to ensure <br/>
+            committed reliable volunteers for our <br/>
+            organizations.
+            </p>
+            <Link to="/signup">
+              <Button className={'ui footer button bot'} size={'massive'}>Join Now!</Button>
+            </Link>
+          </Container>
+        </Grid>
+        <Grid item sm={1}>
+          <Image src='https://cdn.discordapp.com/attachments/441780757967405067/943854340714676284/unknown.png'></Image>
+        </Grid>
+      </Grid>
+    </Container>
     <Container textAlign="center" fluid>
       <Link to="/org-library">
-        <Button className={'ui footer button'} size={'massive'}>Check Out Our Latest Opportunities</Button>
+        <Button className={'ui footer button bot'} size={'massive'}>Check Out Our Latest Opportunities</Button>
       </Link>
     </Container>
     <Footer/>
