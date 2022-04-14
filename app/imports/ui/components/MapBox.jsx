96694f90d1c5650377528d1f4548de0c1837ba3d
Jonathan-Ma <jma66@hawaii.edu>
2022-04-14 12:00:43 -1000

    map tweak

--- app/imports/ui/components/MapBox.jsx
+++ app/imports/ui/components/MapBox.jsx
@@ -1,25 +1,45 @@
 import * as React from 'react';
-import Map from 'react-map-gl';
+import Map, { Source, Layer } from 'react-map-gl';
 
 import { withRouter } from 'react-router-dom';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
 
+const geojson = {
+  type: 'FeatureCollection',
+  features: [
+    { type: 'Feature', geometry: { type: 'Point', coordinates: [-122.4, 37.8] } },
+  ],
+};
+
+const layerStyle = {
+  id: 'point',
+  type: 'circle',
+  paint: {
+    'circle-radius': 10,
+    'circle-color': '#007cbf',
+  },
+};
+
 function App() {
+  const [viewport, setViewport] = React.useState();
   return (
     <Map
       initialViewState={{
         longitude: -122.4,
         latitude: 37.8,
         zoom: 14,
-      }}
+      }}>
+      <Source id="my-data" type="geojson" data={geojson}>
       style={{ width: 100, height: 100 }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+        <Layer {...layerStyle}/>
+      </Source>
+    </Map>
   );
 }
 const MapBox = () => (
   // Important! Always set the container height explicitly
-  <div id={COMPONENT_IDS.FILTER_OPPORTUNITIES_MAP} className={'google-map-border'}>
+  <div id={COMPONENT_IDS.FILTER_OPPORTUNITIES_MAP} className={'map-border'}>
     <App/>
   </div>
 );
