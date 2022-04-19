8ea8f26555244e4d6c55f025e90cc2485604a54b
marcos buccat <buccatm@hawaii.edu>
2022-04-18 14:15:39 -1000

    Implemented the buttons for each cards

--- app/imports/ui/pages/ConfirmVolunteersPage.jsx
+++ app/imports/ui/pages/ConfirmVolunteersPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header, Loader, Input, List, Button, Card, Icon, Grid } from 'semantic-ui-react';
+import { Container, Header, Loader, Input, List, Button, Icon, Grid } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { PAGE_IDS } from '../utilities/PageIDs';
@@ -10,39 +10,55 @@ import { OpportunityRsvps } from '../../api/opportunity/OpportunitiesRsvpCollect
 import { Opportunities } from '../../api/opportunity/OpportunityCollection';
 
 /** Renders a page containing all of the Users reserving for an event. Use <ConfirmVolunteerCard> to render each cards. */
-const ConfirmVolunteersPage = ({ ready, finalData }) => ((ready) ? (
-  <Container id={PAGE_IDS.CONFIRM_HOURS}>
-    <Header as="h2" textAlign="center">Confirm Hours</Header>
-    <Input fluid placeholder="Search for Registered Volunteers..."/>
-    <List horizontal style={{ paddingBottom: '20px' }}>
-      <List.Item>
-        <Header as='h4' style={{ paddingTop: '8px', width: '70px' }}>Filter By: </Header>
-      </List.Item>
-      <List.Item>
-        <Button compact size='small'>A-Z</Button>
-      </List.Item>
-    </List>
-    <Grid columns={3} stackable>
-      {finalData.map((data) => <Grid.Column style={{ marginBottom: '8px' }} key={data.index}>
-        <ConfirmHoursCard key={data.index} linkData={data}/>
-        <Container style={{ paddingLeft: '89px' }}>
-          <Button positive onClick={() => console.log(data)}>
-            Approve
-          </Button>
-          <Button negative onClick={() => console.log(data)}>
-            Decline
-          </Button>
-        </Container>
-      </Grid.Column>)}
-    </Grid>
+const ConfirmVolunteersPage = ({ ready, finalData }) => {
+  const acceptData = [];
+  const declineData = [];
 
-    <Button floated={'right'} icon labelPosition='right'>
-      <Icon name='right arrow'/>
+  const confirming = (data) => {
+    acceptData.push(data);
+  };
+
+  const declining = (data) => {
+    declineData.push(data);
+    console.log(declineData);
+  };
+
+  const finalConfirmation = (data) => {
+    console.log(data);
+  }
+  return ((ready) ? (
+    <Container id={PAGE_IDS.CONFIRM_HOURS}>
+      <Header as="h2" textAlign="center">Confirm Hours</Header>
+      <Input fluid placeholder="Search for Registered Volunteers..."/>
+      <List horizontal style={{ paddingBottom: '20px' }}>
+        <List.Item>
+          <Header as='h4' style={{ paddingTop: '8px', width: '70px' }}>Filter By: </Header>
+        </List.Item>
+        <List.Item>
+          <Button compact size='small'>A-Z</Button>
+        </List.Item>
+      </List>
+      <Grid columns={3} stackable>
+        {finalData.map((data) => <Grid.Column style={{ marginBottom: '8px' }} key={data.index}>
+          <ConfirmHoursCard key={data.index} linkData={data}/>
+          <Container style={{ paddingLeft: '90px', marginTop: '0px' }}>
+            <Button positive onClick={() => confirming(data)}>
+            Approve
+            </Button>
+            <Button negative onClick={() => declining(data)}>
+            Decline
+            </Button>
+          </Container>
+        </Grid.Column>)}
+      </Grid>
+      <Button floated={'right'} icon labelPosition='right' onClick={() => finalConfirmation(acceptData)}>
+        <Icon name='right arrow'/>
           Confirm Volunteer Registration
-    </Button>
-    <Footer2/>
-  </Container>
-) : <Loader active>Getting User Data!</Loader>);
+      </Button>
+      <Footer2/>
+    </Container>
+  ) : <Loader active>Getting User Data!</Loader>);
+};
 
 // Require an array of Stuff documents in the props.
 ConfirmVolunteersPage.propTypes = {
